d5a1ed522223358e5bac730e1bbfc72e6f0edbdd
Kleeedolinux <nicolasketion@gmail.com>
2025-12-04 23:56:36 -0300

    feat: Allow dot characters in player usernames and reformat data constraint predicate definitions.

--- common/src/main/java/me/lucko/luckperms/common/storage/misc/DataConstraints.java
+++ common/src/main/java/me/lucko/luckperms/common/storage/misc/DataConstraints.java
@@ -29,7 +29,8 @@ import java.util.function.Predicate;
 import java.util.regex.Pattern;
 
 public final class DataConstraints {
-    private DataConstraints() {}
+    private DataConstraints() {
+    }
 
     public static final int MAX_PERMISSION_LENGTH = 200;
 
@@ -37,26 +38,33 @@ public final class DataConstraints {
     public static final int MAX_GROUP_NAME_LENGTH = 36;
 
     public static final int MAX_PLAYER_USERNAME_LENGTH = 16;
-    public static final Pattern PLAYER_USERNAME_INVALID_CHAR_MATCHER = Pattern.compile("[^A-Za-z0-9_]");
+    public static final Pattern PLAYER_USERNAME_INVALID_CHAR_MATCHER = Pattern.compile("[^A-Za-z0-9_.]");
 
     public static final int MAX_SERVER_LENGTH = 36;
     public static final int MAX_WORLD_LENGTH = 36;
 
     public static final Predicate<String> PERMISSION_TEST = s -> !s.isEmpty() && s.length() <= MAX_PERMISSION_LENGTH;
 
-    public static final Predicate<String> PLAYER_USERNAME_TEST = s -> !s.isEmpty() && s.length() <= MAX_PLAYER_USERNAME_LENGTH && !PLAYER_USERNAME_INVALID_CHAR_MATCHER.matcher(s).find();
+    public static final Predicate<String> PLAYER_USERNAME_TEST = s -> !s.isEmpty()
+            && s.length() <= MAX_PLAYER_USERNAME_LENGTH && !PLAYER_USERNAME_INVALID_CHAR_MATCHER.matcher(s).find();
 
-    public static final Predicate<String> PLAYER_USERNAME_TEST_LENIENT = s -> !s.isEmpty() && s.length() <= MAX_PLAYER_USERNAME_LENGTH;
+    public static final Predicate<String> PLAYER_USERNAME_TEST_LENIENT = s -> !s.isEmpty()
+            && s.length() <= MAX_PLAYER_USERNAME_LENGTH;
 
-    public static final Predicate<String> GROUP_NAME_TEST = s -> !s.isEmpty() && s.length() <= MAX_GROUP_NAME_LENGTH && !s.contains(" ");
+    public static final Predicate<String> GROUP_NAME_TEST = s -> !s.isEmpty() && s.length() <= MAX_GROUP_NAME_LENGTH
+            && !s.contains(" ");
 
-    public static final Predicate<String> GROUP_NAME_TEST_ALLOW_SPACE = s -> !s.isEmpty() && s.length() <= MAX_GROUP_NAME_LENGTH;
+    public static final Predicate<String> GROUP_NAME_TEST_ALLOW_SPACE = s -> !s.isEmpty()
+            && s.length() <= MAX_GROUP_NAME_LENGTH;
 
-    public static final Predicate<String> TRACK_NAME_TEST = s -> !s.isEmpty() && s.length() <= MAX_TRACK_NAME_LENGTH && !s.contains(" ");
+    public static final Predicate<String> TRACK_NAME_TEST = s -> !s.isEmpty() && s.length() <= MAX_TRACK_NAME_LENGTH
+            && !s.contains(" ");
 
-    public static final Predicate<String> TRACK_NAME_TEST_ALLOW_SPACE = s -> !s.isEmpty() && s.length() <= MAX_TRACK_NAME_LENGTH;
+    public static final Predicate<String> TRACK_NAME_TEST_ALLOW_SPACE = s -> !s.isEmpty()
+            && s.length() <= MAX_TRACK_NAME_LENGTH;
 
-    public static final Predicate<String> SERVER_NAME_TEST = s -> !s.isEmpty() && s.length() <= MAX_SERVER_LENGTH && !s.contains(" ");
+    public static final Predicate<String> SERVER_NAME_TEST = s -> !s.isEmpty() && s.length() <= MAX_SERVER_LENGTH
+            && !s.contains(" ");
 
     public static final Predicate<String> WORLD_NAME_TEST = s -> !s.isEmpty() && s.length() <= MAX_WORLD_LENGTH;
 
